0d3bc6c8516bb49a2ccf17c69d364e2f8afec96d
benjym <benjy.marks@sydney.edu.au>
2020-09-22 12:40:12 +1000

    added Francois to credits

--- js/slope-stability.js
+++ js/slope-stability.js
@@ -9,7 +9,7 @@ var map = L.map('map', {
 L.control.scale({ position: "bottomright" }).addTo(map); // add scale bar
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a> | Brought together by <a href="https://www.benjymarks.com">Benjy Marks</a>',
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a> | Brought together by <a href="https://www.benjymarks.com">Benjy Marks</a> and <a href="http://francoisguillard.com/">François Guillard</a>',
     maxZoom: 22,
     id: 'benjymarks/ckekrzken0dfj19nt7y93p671',
     tileSize: 512,
